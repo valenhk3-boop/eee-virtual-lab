e94e0912acba9bce66d6196a6cd8d0d1035d1294
Valen <valenhk3@gmail.com>
2026-03-30 14:38:06 +0530

    Update __init__.py

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,1 +1,11 @@
+import numpy as np
 
+def add_noise(value, noise_level=0.005):
+    return value + np.random.normal(0, noise_level)
+
+def apply_tolerance(nominal_value, tolerance=0.05):
+    return nominal_value * np.random.uniform(1 - tolerance, 1 + tolerance)
+
+def calculate_loading(v_in, r_top, r_bottom, r_load):
+    r_eff = (r_bottom * r_load) / (r_bottom + r_load)
+    return v_in * (r_eff / (r_top + r_eff))
